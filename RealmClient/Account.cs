fb1b605d3c856783279d5c5dfab93c01c987fc2e
kecleon <ultimatedood@gmail.com>
2022-12-12 01:19:01 -0800

    moved urls to account, fixed url request

--- RealmClient/Account.cs
+++ RealmClient/Account.cs
@@ -41,5 +41,109 @@ public class Account {
 		ClientToken = Guid.Hash().ToHexString();
 	}
 
+	public void LoadLauncherUrls() {
+		var appInit = Web.Load("/app/init?platform=standalonewindows64&key=9KnJFxtTvLu2frXv", Web.EmptyParameters);
+		appInit = Web.Load("/app/init?platform=standalonewindows64&key=9KnJFxtTvLu2frXv", new Dictionary<string, string> {
+			{ "game_net", "rotmg" },
+		});
+		var verify = Web.Load("/account/verify", new Dictionary<string, string> {
+			{ "guid", Uri.EscapeDataString(Guid) },
+			{ "password", Uri.EscapeDataString(Password) },
+			{ "clientToken", ClientToken },
+			{ "currentToken", Uri.EscapeDataString(AccessToken) },
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		Web.Load("/build/toolsVersion", new Dictionary<string, string> {
+			{ "guid", Uri.EscapeDataString(Guid) },
+			{ "platform", "standaloneosxuniversal" },
+			{ "clientToken", ClientToken },
+			{ "currentToken", Uri.EscapeDataString(AccessToken) },
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		appInit = Web.Load("/app/init?platform=standalonewindows64&key=9KnJFxtTvLu2frXv", new Dictionary<string, string> {
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var news = Web.Load("/unityNews/getNews", new Dictionary<string, string> {
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var servers = Web.Load("/account/servers", new Dictionary<string, string> {
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var status = Web.Load("/serverStatus/getServerStatus", new Dictionary<string, string> {
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+	}
+
+	public void LoadLauncherPlayUrls() {
+		var verify = Web.Load("/account/verify", new Dictionary<string, string> {
+			{ "guid", Uri.EscapeDataString(Guid) },
+			{ "clientToken", ClientToken },
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var verifyAccessToken = Web.Load("/account/verify", new Dictionary<string, string> {
+			{ "clientToken", ClientToken },
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+	}
+
+	public void LoadClientUrls() {
+		var appInit = Web.Load("/app/init", Web.EmptyParameters);
+		var verifyAccessToken = Web.Load("/account/verifyAccessTokenClient", new Dictionary<string, string> {
+			{ "clientToken", ClientToken },
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var charList = Web.Load("/char/list", new Dictionary<string, string> {
+			{ "do_login", "true" },
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var ownedPetSkins = Web.Load("/account/getOwnedPetSkins", new Dictionary<string, string> {
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var campaignStatus = Web.Load("/supportCampaign/status", new Dictionary<string, string> {
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var dungeonEvents = Web.Load("/dungeonEvent/getClientEvents", new Dictionary<string, string> {
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var shopDeals = Web.Load("/shop/deals", new Dictionary<string, string> {
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var seasonInfo = Web.Load("/season/info", new Dictionary<string, string> {
+			{ "language", "en" },
+			{ "version", "0" },
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var activeLeaderboard = Web.Load("/leaderboards/getBoardActive", new Dictionary<string, string> {
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var dailyLoginCalendar = Web.Load("/dailyLogin/fetchCalendar", new Dictionary<string, string> {
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var accountList0 = Web.Load("/account/list", new Dictionary<string, string> {
+			{ "type", "0" }, //todo: account list constants
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var friendRequests = Web.Load("/friends/getRequests", new Dictionary<string, string> {
+			{ "targetName", "" },
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var accountList1 = Web.Load("/account/list", new Dictionary<string, string> {
+			{ "type", "1" }, //todo: account list constants
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var publicStatDataPowerUp = Web.Load("/app/publicStaticData", new Dictionary<string, string> {
+			{ "dataType", "powerUpSettings" },
+			{ "version", "0" },
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var powerUpStats = Web.Load("/account/listPowerUpStats", new Dictionary<string, string> {
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+		var publicStatDataKeyRefine = Web.Load("/app/publicStaticData", new Dictionary<string, string> {
+			{ "dataType", "keyRefineSettings" },
+			{ "version", "0" },
+			{ "accessToken", Uri.EscapeDataString(AccessToken) },
+		});
+	}
 	}
 }
--- RealmClient/Util/Web.cs
+++ RealmClient/Util/Web.cs
@@ -17,7 +17,6 @@ public class Web {
 	private static WebHeaderCollection DefaultHeaders = new() {
 		{"Accept", "*/*"},
 		{"Accept-Encoding", "deflate, gzip"},
-		{"Content-Type", "application/x-www-form-urlencoded"},
 	};
 
 	//present in exalt launcher
@@ -58,140 +57,19 @@ public class Web {
 		}
 	}
 
-	public static void LoadLauncherUrls(Account account) {
-		var appInit = Load("/app/init?platform=standalonewindows64&key=9KnJFxtTvLu2frXv", EmptyParameters);
-		appInit = Load("/app/init?platform=standalonewindows64&key=9KnJFxtTvLu2frXv", new Dictionary<string, string> {
-			{ "game_net", "rotmg" },
-		});
-		var verify = Load("/account/verify", new Dictionary<string, string> {
-			{ "guid", Uri.EscapeDataString(account.Guid) },
-			{ "password", Uri.EscapeDataString(account.Password) },
-			{ "clientToken", account.ClientToken },
-			{ "currentToken", Uri.EscapeDataString(account.AccessToken) },
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		Load("/build/toolsVersion", new Dictionary<string, string> {
-			{ "guid", Uri.EscapeDataString(account.Guid) },
-			{ "platform", "standaloneosxuniversal" },
-			{ "clientToken", account.ClientToken },
-			{ "currentToken", Uri.EscapeDataString(account.AccessToken) },
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		appInit = Load("/app/init?platform=standalonewindows64&key=9KnJFxtTvLu2frXv", new Dictionary<string, string> {
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var news = Load("/unityNews/getNews", new Dictionary<string, string> {
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var servers = Load("/account/servers", new Dictionary<string, string> {
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var status = Load("/serverStatus/getServerStatus", new Dictionary<string, string> {
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-	}
-
-	public static void LoadLauncherPlayUrls(Account account) {
-		var verify = Load("/account/verify", new Dictionary<string, string> {
-			{ "guid", Uri.EscapeDataString(account.Guid) },
-			{ "clientToken", account.ClientToken },
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var verifyAccessToken = Load("/account/verify", new Dictionary<string, string> {
-			{ "clientToken", account.ClientToken },
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-	}
-
-	public static void LoadClientUrls(Account account) {
-		var appInit = Load("/app/init", EmptyParameters);
-		var verifyAccessToken = Load("/account/verifyAccessTokenClient", new Dictionary<string, string> {
-			{ "clientToken", account.ClientToken },
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var charList = Load("/char/list", new Dictionary<string, string> {
-			{ "do_login", "true" },
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var ownedPetSkins = Load("/account/getOwnedPetSkins", new Dictionary<string, string> {
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var campaignStatus = Load("/supportCampaign/status", new Dictionary<string, string> {
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var dungeonEvents = Load("/dungeonEvent/getClientEvents", new Dictionary<string, string> {
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var shopDeals = Load("/shop/deals", new Dictionary<string, string> {
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var seasonInfo = Load("/season/info", new Dictionary<string, string> {
-			{ "language", "en" },
-			{ "version", "0" },
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var activeLeaderboard = Load("/leaderboards/getBoardActive", new Dictionary<string, string> {
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var dailyLoginCalendar = Load("/dailyLogin/fetchCalendar", new Dictionary<string, string> {
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var accountList0 = Load("/account/list", new Dictionary<string, string> {
-			{ "type", "0" }, //todo: account list constants
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var friendRequests = Load("/friends/getRequests", new Dictionary<string, string> {
-			{ "targetName", "" },
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var accountList1 = Load("/account/list", new Dictionary<string, string> {
-			{ "type", "1" }, //todo: account list constants
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var publicStatDataPowerUp = Load("/app/publicStaticData", new Dictionary<string, string> {
-			{ "dataType", "powerUpSettings" },
-			{ "version", "0" },
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var powerUpStats = Load("/account/listPowerUpStats", new Dictionary<string, string> {
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-		var publicStatDataKeyRefine = Load("/app/publicStaticData", new Dictionary<string, string> {
-			{ "dataType", "keyRefineSettings" },
-			{ "version", "0" },
-			{ "accessToken", Uri.EscapeDataString(account.AccessToken) },
-		});
-	}
-
 	public static Dictionary<string, string> EmptyParameters = new();
 
 	public static async Task<string> Load(string path, Dictionary<string, string> parameters)
 	{
 		// Create a POST request with the specified URI
 		HttpRequestMessage request = new(HttpMethod.Post, path);
-		//server denies HEAD methods, would've been nice to skip loading useless things like unityNews
 
-		// Create a string builder to hold the parameters
-		StringBuilder sb = new();
-
-		// Loop through the parameters in the dictionary
-		foreach (KeyValuePair<string, string> parameter in parameters) {
-			// Add the parameter to the string builder
-			sb.Append(parameter.Key + "=" + parameter.Value + "&");
+		//use form url encoded content type, because HttpClient doesn't let you manually specify the Content-Type header
+		foreach (KeyValuePair<string, string> parameter in DefaultParameters) {
+			parameters.Add(parameter.Key, parameter.Value);
 		}
 
-		//add default parameters
-		foreach (KeyValuePair<string, string> parameter in DefaultParameters)
-		{
-			// Add the parameter to the string builder
-			sb.Append(parameter.Key + "=" + parameter.Value + "&");
-		}
-
-		// Remove the last trailing &
-		sb.Remove(sb.Length - 1, 1);
-
-		// Set the content of the request
-		request.Content = new StringContent(sb.ToString());
+		request.Content = new FormUrlEncodedContent(parameters);
 
 		// Send the request to the server
 		HttpResponseMessage response = await Client.SendAsync(request);
